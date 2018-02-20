ca1523e64354f46fe849b7c33b0ce3d80219300b
Camberos42 <35781327+Camberos42@users.noreply.github.com>
2018-02-20 13:47:46 -0800

    Delete IProvedor.java

--- 18_02_14_Practica3/IProvedor.java
+++
@@ -1,7 +0,0 @@
-public interface IProvedor {
-	public void AgregarC(Cliente c);
-	public void Removerc(Cliente c);
-	public void notifyUsers();
-	public void AgregarPaquete(Cliente c,String art);
-
-}
