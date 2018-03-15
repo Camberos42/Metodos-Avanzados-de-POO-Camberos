516e1dc680212a8020de5826e747fb1e6d13ca0f
Camberos42 <35781327+Camberos42@users.noreply.github.com>
2018-03-14 19:34:24 -0700

    Add files via upload

---
+++ 180314_EjercicioFactory/PlayasPizzaHawaiana.java
@@ -0,0 +1,15 @@
+public class PlayasPizzaHawaiana extends Pizza {
+	
+	public PlayasPizzaHawaiana() {
+		nombre = "Hawaiana Especial Playas";
+		masa = "Delgada";
+		salsa = "Salsa de tomate especial";
+ 
+		Ingredientes.add("Queso Mozzarela");
+		Ingredientes.add("Queso Cheddar");
+		Ingredientes.add("Jamon");
+		Ingredientes.add("Piña");
+		Ingredientes.add("Champiñones");
+	}
+
+}
---
+++ 180314_EjercicioFactory/PlayasPizzaPeperoni.java
@@ -0,0 +1,14 @@
+public class PlayasPizzaPeperoni extends Pizza{
+	
+	public PlayasPizzaPeperoni() {
+		nombre = "Pepperoni Playas Pizza";
+		masa = "Orilla de ajo";
+		salsa = "Salsa de tomate";
+ 
+		Ingredientes.add("Queso Mozzarella");
+		Ingredientes.add("Pepperoni");
+		Ingredientes.add("Queso Cheddar");
+		Ingredientes.add("Red Pepper");
+	}
+
+}
